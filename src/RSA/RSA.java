4111e4c5892d00a9c022314ac865326da9042043
Chris <ctownsle@oswego.edu>
2018-10-14 17:05:01 -0400

    added RSA class to handle RSA encryption/decryption

---
+++ src/RSA/RSA.java
@@ -0,0 +1,32 @@
+package RSA;
+
+import java.math.BigInteger;
+import java.util.Random;
+
+public class RSA {
+
+    private Random r;
+    private BigInteger q;
+    private BigInteger p;
+    private BigInteger N;
+    private BigInteger phi;
+    private BigInteger e;
+    private BigInteger d;
+    private static final int bitlength = 1024;
+    /*
+    So far this program does calculations for finding a random p, q and n with bitlength of 1024
+    I'll leave it at this for now
+    */
+    public RSA(){
+
+        r = new Random();
+        p = BigInteger.probablePrime(bitlength, r);
+        q = BigInteger.probablePrime(bitlength, r);
+        N = p.multiply(q);
+
+        // calculate phi
+        phi = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+
+        e = BigInteger.probablePrime(bitlength/2, r);
+    }
+}
